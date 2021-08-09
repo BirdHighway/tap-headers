cc123e2e8b29adbe70a9f07709f20efd9d074b71
Stephen Hyde <shh2112@gmail.com>
2021-08-09 14:55:57 -0400

    Remove previous version files and install bootstrap

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import connection from '../websockets';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 const App = () => {
-
+  return (
+    <h1>Hello</h1>
+  )
 };
 
 export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
+import store from './components/store';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('app')
 );
